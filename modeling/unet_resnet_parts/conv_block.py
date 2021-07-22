3310481d416c4b70ad3ffd92c4cb2e09f4ba55aa
rooncal <ron.kal8@gmail.com>
2021-07-22 08:59:30 +0300

    Fixed bn syntax error and imports

--- modeling/unet_resnet_parts/conv_block.py
+++ modeling/unet_resnet_parts/conv_block.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 class convBlock(nn.Module):
     def __init__(self, in_channels, filters, size, stride = 1, activation = True, use_bn=True):
         super(convBlock, self).__init__()
+        self.use_bn = use_bn
         self.activation = activation
         self.conv = nn.Conv2d(in_channels, filters, size, stride = stride, padding = size//2)
         self.norm = nn.BatchNorm2d(filters)
--- modeling/unet_resnet_parts/residual_block.py
+++ modeling/unet_resnet_parts/residual_block.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from conv_block import convBlock
 
 class residualBlock(nn.Module):
     def __init__(self, in_channels, filters, size = 3, use_bn=True):
@@ -8,11 +9,12 @@ class residualBlock(nn.Module):
         self.conv1 = convBlock(in_channels, filters, size, use_bn=use_bn)
         self.conv2 = convBlock(filters, filters, size, activation=False, use_bn=use_bn)
         self.relu = nn.ReLU()
+        self.use_bn = use_bn
 
     def forward(self, x):
         residual = x  
         x = self.relu(x)
-        if use_bn:
+        if self.use_bn:
           x = self.norm(x)
         x = self.conv1(x)
         x = self.conv2(x)
