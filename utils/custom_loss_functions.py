76243eced061a333227b7f7c50f839136348d64d
rooncal <ron.kal8@gmail.com>
2021-07-28 18:19:27 +0300

    Change variables in ComboLoss to self variables

--- utils/custom_loss_functions.py
+++ utils/custom_loss_functions.py
@@ -40,13 +40,13 @@ class FocalLoss(nn.Module):
 
 
 class ComboLoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
+    def __init__(self, weight=None, size_average=True, alpha=0.5, ce_ratio=0.5):
         super(ComboLoss, self).__init__()
+        self.alpha = alpha
+        self.ce_ratio = ce_ratio
+        self.e = 1e-07
 
-    def forward(self, inputs, targets, smooth=1, alpha=ALPHA): 
-        ALPHA = 0.5 # < 0.5 penalises FP more, > 0.5 penalises FN more
-        CE_RATIO = 0.5 #weighted contribution of modified CE loss compared to Dice loss
-        e = 1e-07
+    def forward(self, inputs, targets, smooth=1): 
 
         inputs = torch.sigmoid(inputs)
         
@@ -58,9 +58,9 @@ class ComboLoss(nn.Module):
         intersection = (inputs * targets).sum()    
         dice = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
         
-        inputs = torch.clamp(inputs, e, 1.0 - e)
-        out = - (ALPHA * ((targets * torch.log(inputs)) + ((1 - ALPHA) * (1.0 - targets) * torch.log(1.0 - inputs))))
+        inputs = torch.clamp(inputs, self.e, 1.0 - self.e)
+        out = - (self.alpha * ((targets * torch.log(inputs)) + ((1 - self.alpha) * (1.0 - targets) * torch.log(1.0 - inputs))))
         weighted_ce = out.mean(-1)
-        combo = (CE_RATIO * weighted_ce) - ((1 - CE_RATIO) * dice)
+        combo = (self.ce_ratio * weighted_ce) - ((1 - self.ce_ratio) * dice)
         
         return combo
